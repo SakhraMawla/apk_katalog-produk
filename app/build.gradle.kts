72daac3c23067961c524b1a5cbb47c2ea26a691c
SakhraMawla <sakhramawla@gmail.com>
2026-05-09 20:01:40 +0700

    Menambahkan foto produk baru Menambahkan foto produk baru ke folder drawableke folder drawable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
