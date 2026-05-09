22c8b1062caf6237157a61bb520aa059c1f71284
miftahrizqi-coder <miftahrizqi18@gmail.com>
2026-05-09 15:10:15 +0700

    Pembuatan adapter, viewholder dan tes pada main activity

--- app/src/main/java/com/example/apk_katalogproduk/activity/MainActivity.kt
+++ app/src/main/java/com/example/apk_katalogproduk/activity/MainActivity.kt
@@ -1,2 +1,59 @@
 package com.example.apk_katalogproduk.activity
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.apk_katalogproduk.R
+import com.example.apk_katalogproduk.adapter.ProductAdapter
+import com.example.apk_katalogproduk.model.Product
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: ProductAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.recyclerView)
+
+        val listProduct: List<Product> = listOf(
+
+            Product(
+                1,
+                "Milo",
+                "Kucing Persia",
+                450000.00,
+                4.5f,
+                R.drawable.ic_launcher_foreground,
+                "Kucing lucu dan aktif"
+            ),
+
+            Product(
+                2,
+                "Oyen",
+                "Kucing Anggora",
+                450000.00,
+                4.0f,
+                R.drawable.ic_launcher_foreground,
+                "Kucing lucu dan aktif"
+            ),
+
+            Product(
+                3,
+                "Bobi",
+                "Anjing Husky",
+                450000.00,
+                5.0f,
+                R.drawable.ic_launcher_foreground,
+                "Anjing lucu dan aktif"
+            )
+        )
+
+        adapter = ProductAdapter(listProduct)
+        recyclerView.adapter = adapter
+    }
+}
+
--- app/src/main/java/com/example/apk_katalogproduk/viewholder/ProductViewHolder.kt
+++ app/src/main/java/com/example/apk_katalogproduk/viewholder/ProductViewHolder.kt
@@ -1,4 +1,20 @@
 package com.example.apk_katalogproduk.viewholder
 
-class ProductViewHolder {
+import android.view.View
+import android.widget.ImageView
+import android.widget.RatingBar
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.apk_katalogproduk.R
+
+class ProductViewHolder(itemView: View)
+    : RecyclerView.ViewHolder(itemView) {
+
+    val imgPet: ImageView = itemView.findViewById(R.id.imgPet)
+    val txtId: TextView = itemView.findViewById(R.id.txtId)
+    val txtName: TextView = itemView.findViewById(R.id.txtName)
+    val txtType: TextView = itemView.findViewById(R.id.txtType)
+    val txtDesc: TextView = itemView.findViewById(R.id.txtDesc)
+    val txtPrice: TextView = itemView.findViewById(R.id.txtPrice)
+    val ratingPet: RatingBar = itemView.findViewById(R.id.ratingPet)
 }
